7940dc18bf00aab3901dc8080fc7ae016f484a74
jflett1990 <81728344+jflett1990@users.noreply.github.com>
2024-01-15 03:03:43 -0500

    Create local_content_manager.py

---
+++ utilities/local_content_manager.py
@@ -0,0 +1,16 @@
+import os
+import json
+
+class LocalContentManager:
+    def __init__(self):
+        self.base_path = 'agent_content'
+        self.ensure_base_path()
+
+    def ensure_base_path(self):
+        if not os.path.exists(self.base_path):
+            os.makedirs(self.base_path)
+
+    def save_content(self, content_type, file_name, content):
+        path = os.path.join(self.base_path, content_type, file_name)
+        with open(path, 'w') as file:
+            if content_type ==
